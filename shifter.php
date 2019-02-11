359fe8044d8b51eeeb66b2afeee4620ae84bdde8
Alexander Menk <a.menk@imi.de>
2019-02-11 17:51:04 +0100

    Refactoring

--- shifter.php
+++ shifter.php
@@ -40,26 +40,33 @@ class Shifter
 
     public function execute()
     {
-        $this->authenticate();
-        $this->ensureTemporaryRepositoryExists();
-        $this->push();
-        $this->sayHowToShift();
-
+        try {
+            $this->authenticate();
+            $this->ensureTemporaryRepositoryExists();
+            $this->push();
+            $this->sayHowToShift();
+        } catch (\Exception $exception) {
+            echo sprintf('ERROR: %s @ %s:%d',
+                    $exception->getMessage(),
+                    $exception->getFile(),
+                    $exception->getLine()) . PHP_EOL;
+        }
     }
+
     protected function authenticate()
     {
         $tokenFile = __DIR__ . '/.github_token';
 
         $token = trim(@file_get_contents($tokenFile));
 
-        if (!$token) {
+        if ( ! $token) {
             echo "Go to https://github.com/settings/tokens , create a token for repo access and repo_delete and put the token into $tokenFile" . PHP_EOL;
             die();
         }
 
         $this->gitHub->authenticate($token, '', \Github\Client::AUTH_HTTP_TOKEN);
 
-        $this->userName = $this->gitHub->api('current_user')['login'];
+        $this->userName = $this->gitHub->api('current_user')->show()['login'];
     }
 
     protected function ensureTemporaryRepositoryExists()
@@ -71,8 +78,8 @@ class Shifter
                 throw \Exception('Refusing to work on public repos.');
             }
         } else {
-            $this->temporaryRepo  = $this->gitHub->api('repo')->create($temporaryRepoName,
-                'Shifter Temporary Repo, can be deleted after shifting', $sourceRepo, false);
+            $this->temporaryRepo = $this->gitHub->api('repo')->create($this->temporaryRepoName,
+                'Shifter Temporary Repo, can be deleted after shifting', '', false);
         }
     }
 
@@ -98,7 +105,7 @@ class Shifter
 
     protected function sayHowToShift()
     {
-        echo "Go to https://laravelshift.com/shifts purchase a shift and enter the following repo name:" . PHP_EOL;
+        echo "Now please go to https://laravelshift.com/shifts purchase a shift and enter the following repo name:" . PHP_EOL;
         echo $this->temporaryRepo['full_name'] . PHP_EOL;
         echo "And this branch:" . PHP_EOL;
         echo $this->currentBranch;
